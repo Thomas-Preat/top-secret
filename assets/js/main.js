e59f87f0377c08e5b173dce0a180e9b781ae5fdf
Thomas-Preat <preatthomas@gmail.com>
2025-12-16 01:42:14 +0100

    Initialize Firebase in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,25 @@ if (navToggle && navMenu) {
         });
     });
 }
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCPawC7OPXmvzu2Fj8Uzfj1ZXQI3v-6VL8",
+  authDomain: "topsecret-9ae10.firebaseapp.com",
+  projectId: "topsecret-9ae10",
+  storageBucket: "topsecret-9ae10.firebasestorage.app",
+  messagingSenderId: "946145195302",
+  appId: "1:946145195302:web:a25c392b7efde11137fde7",
+  measurementId: "G-6E69WMM89W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

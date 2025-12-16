69991b9e65ca80d002f8a64c121341ec6f9a4a9c
Thomas-Preat <preatthomas@gmail.com>
2025-12-16 02:09:06 +0100

    Refactor Firebase imports and reorganize code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-//function to select elements
-const $ = (sel, ctx = document) => ctx.querySelector(sel);
-const $all = (sel, ctx = document) => [...ctx.querySelectorAll(sel)];
-
-//menu toggle
-const navToggle = $('.nav-toggle');
-const navMenu = $('#primary-nav');
-
-if (navToggle && navMenu) {
-    console.log('ici');
-    navToggle.addEventListener('click', () => {
-        console.log('click');
-        const expanded = navToggle.getAttribute('aria-expanded') === 'true';
-        navToggle.setAttribute('aria-expanded', !expanded);
-        navMenu.classList.toggle('show');
-    });
-
-    // close menu when clicking a link
-    $all('#primary-nav a').forEach(link => {
-        link.addEventListener('click', () => {
-            console.log('click link');
-            navToggle.setAttribute('aria-expanded', false);
-            navMenu.classList.remove('show');
-        });
-    });
-}
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -55,3 +27,32 @@ const snapshot = await getDocs(checklistRef);
 snapshot.forEach(d => {
     console.log(d.id, d.data());
 });
+
+//-----------------------------------------------------
+
+//function to select elements
+const $ = (sel, ctx = document) => ctx.querySelector(sel);
+const $all = (sel, ctx = document) => [...ctx.querySelectorAll(sel)];
+
+//menu toggle
+const navToggle = $('.nav-toggle');
+const navMenu = $('#primary-nav');
+
+if (navToggle && navMenu) {
+    console.log('ici');
+    navToggle.addEventListener('click', () => {
+        console.log('click');
+        const expanded = navToggle.getAttribute('aria-expanded') === 'true';
+        navToggle.setAttribute('aria-expanded', !expanded);
+        navMenu.classList.toggle('show');
+    });
+
+    // close menu when clicking a link
+    $all('#primary-nav a').forEach(link => {
+        link.addEventListener('click', () => {
+            console.log('click link');
+            navToggle.setAttribute('aria-expanded', false);
+            navMenu.classList.remove('show');
+        });
+    });
+}
